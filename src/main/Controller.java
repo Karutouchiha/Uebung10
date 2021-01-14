23506001de12f867a434803efc12916d35efc0a9
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2021-01-14 16:12:05 +0100

    add Initializable and Attributes to Controller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,23 @@
 package main;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    DataBase DB = new DataBase();
+    @FXML
+    private TextField txtout;
+    @FXML private TextArea txt_lPlate;
+    @FXML private Button quicksearch;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        txtout.setEditable(false);
+    }
 }
