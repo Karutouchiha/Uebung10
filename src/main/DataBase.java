60caf0735296553c0665322a459a54a3c6adee1f
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2021-01-14 16:08:11 +0100

    add DataBase with constructor

---
+++ src/main/DataBase.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.HashMap;
+
+public class DataBase {
+    
+    private HashMap<String,Vehicle> CarDB = new HashMap();
+    private MagicGenerator generator = new MagicGenerator();
+
+    DataBase() {
+        for (int i = 0; i < 20; i++) {
+            String manufacturer = generator.getRandomManufacturer();
+            String licencePlate = generator.getRandomLicencePlate();
+            Vehicle v = new Vehicle(generator.getRandomColor(), generator.getRandomName(), manufacturer, generator.getRandomModel(manufacturer), licencePlate);
+            CarDB.put(licencePlate, v);
+            System.out.print("Licence Plate:" + licencePlate);
+        }
+    }
+}
